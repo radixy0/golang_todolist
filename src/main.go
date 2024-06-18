55b3998e7c24ecd0ba40d09f7550f34106d643b7
radixy0 <ernst.lukas@gmx.net>
2024-06-18 19:25:43 +0200

    hello world

--- src/main.go
+++ src/main.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "hello world")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("listening at localhost:8000/hello")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
